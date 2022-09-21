2800de2d5b620a794649a6060705777153aee0af
Kenzo-Fujisaki <s21C1103BK@s.chibakoudai.jp>
2022-09-21 09:59:20 +0900

    henkou

--- mondai/pra1.cpp
+++ mondai/pra1.cpp
@@ -1,5 +1,33 @@
 #include <iostream>
 
+class Hito{
+private:
+    char name[20];
+    int toshi;
+    char syumi[20];
+public:
+    void input();
+    void show();
+};
+
+void Hito::input(){
+    std::cout << "名前を入力してください：";
+    std::cin >> name;
+    std::cout << "年齢を入力してください：";
+    std::cin >> toshi;
+    std::cout << "趣味を入力してください：";
+    std::cin >> syumi;
+}
+
+void Hito::show(){
+    std::cout << "名前：" << name << std::endl;
+    std::cout << "年齢：" << toshi <<std::endl;
+    std::cout << "趣味：" << syumi <<std::endl;
+}
+
 int main(){
-    
+    Hito h_obj;
+    h_obj.input();
+    h_obj.show();
+    return 0;
 }
